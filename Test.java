8fe6706dc3f6cc8a9ad9e4d6a6d8cfa8acf8eb90
Christian Dalhaug <Christian@Christians-MacBook-Pro.local>
2013-01-27 22:06:44 +0100

    Tester

---
+++ Test.java
@@ -0,0 +1,68 @@
+import javax.swing.JOptionPane.*;
+
+class Person{
+	private String fornavn;
+	private String etternavn;
+	private int alder;
+	
+	public Person(String fornavn, String etternavn, int alder){
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.alder = alder;
+	}
+	
+	public String getFornavn(){
+		return fornavn;
+	}
+	
+	public String getEtternavn(){
+		return etternavn;
+	}
+	
+	public int getAlder(){
+		return alder;
+	}
+	
+	public void setFornavn(String nyttFornavn){
+		fornavn = nyttFornavn;
+	}
+	
+	public void setEtternavn(String nyttEtternavn){
+		etternavn = nyttEtternavn;
+	}
+	
+	public void setAlder(int nyAlder){
+		alder = nyAlder;
+	}
+	
+	public String toString(){
+		return "Fornavn: " + getFornavn() + " Etternavn: " + getEtternavn() + " Alder: " + getAlder();
+	}
+}
+
+
+
+class Test{
+	public static void main(String[]args){
+		
+		String navn = "Christian Dalhaug"; //showInputDialog("Navn: ");
+		String[] navnene = navn.split(" ");
+		String fornavn = "";
+		String etternavn = "";
+		int alder = 22;
+		for(int i = 0; i < navnene.length; i++){
+			if(i == navnene.length - 2){
+				fornavn += navnene[i];
+			}else if(i < navnene.length - 1){
+				fornavn += navnene[i] + " ";
+			}else{
+				etternavn += navnene[i];
+			}	
+		}
+		
+		Person a = new Person(fornavn, etternavn, alder);
+		
+		System.out.println(a.toString());
+		
+	}
+}
